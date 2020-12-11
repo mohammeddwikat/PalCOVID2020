722a1d439d05dc63fa698b88a42a1dbc499d2117
Mohammad Dwikat <mohammed.dwikate1999@gmail.com>
2020-12-11 18:19:16 +0200

    add order class

---
+++ PalCOVID/src/Order.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.time.*;
+
+public class Order {
+	public ArrayList <Item> items ;
+	public String storeName;
+	public String addressDeliver;
+	public String timePlaced;
+	public String PaymentInformation;
+	public int pointGained;
+	public float totalPrice;
+	public int phoneNumber;
+	
+	void showItems() {
+		
+	}
+	void removeItem() {
+		
+	}
+	void setTotalPrice() {
+		// call it when remove an item
+	}
+	
+}
