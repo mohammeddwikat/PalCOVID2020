ec2a7dfb9908049a805eaf9ae995d855d8d32cc6
Mohammad Dwikat <mohammed.dwikate1999@gmail.com>
2020-12-11 18:52:26 +0200

    add delivery company class

---
+++ PalCOVID/src/DeliveryCompany.java
@@ -0,0 +1,26 @@
+
+public class DeliveryCompany {
+	public String name;
+	public int phoneNumber;
+	
+	public DeliveryCompany(String _1, int x) {
+		name = _1;
+		x = phoneNumber;
+	}
+	
+	public void setName(String n) {
+		name = n;
+	}
+	
+	public void setPhoneNumber(int n) {
+		phoneNumber = n;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+}
