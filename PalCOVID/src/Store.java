eb5d504e5738396098d0b7849f882f6cda740d22
Mohammad Dwikat <mohammed.dwikate1999@gmail.com>
2020-12-11 17:55:35 +0200

    Add store class

---
+++ PalCOVID/src/Store.java
@@ -0,0 +1,34 @@
+
+public class Store {
+	
+	public	String name;
+	public String email;
+	public String typeStore;
+	//#Items
+	//Orders
+	//Delivery Compaines 
+	public Store() {
+		
+	}
+	public Store(String _1,String _2,String _3 ) {
+		
+	}
+	public void addItem() {
+		
+	}
+	public void removeItem() {
+		
+	}
+	public void updateItem() {
+		
+	}
+	public void showOrders() {
+		
+	}
+	//add + remove delivery company
+	public void createDeliveryRecord() {
+		
+	}
+	
+	
+}
