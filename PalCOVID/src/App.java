65f922616c9b83a7785cb746c368842d9712a873
Mohammad Dwikat <mohammed.dwikate1999@gmail.com>
2020-12-12 18:13:15 +0200

    add customer

--- PalCOVID/src/App.java
+++ PalCOVID/src/App.java
@@ -3,5 +3,8 @@ public class App {
 	public static void main(String []args) {
 		System.out.println("Hello Covid-19");
 		System.out.println("Hello Covid-19");
+		
+		Customer customer = new Customer ("Crystel", "Dwikat.M@ieee.org", 599149227,"123456*/-", "Nablus", "Jwwal Pay");
+		customer.showDetails();
 	}
 }
