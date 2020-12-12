b7b0ef41eb0d57c88c14988a14582a252a70859a
Mohammad Dwikat <mohammed.dwikate1999@gmail.com>
2020-12-12 18:14:30 +0200

    add another customer

--- PalCOVID/src/App.java
+++ PalCOVID/src/App.java
@@ -4,7 +4,10 @@ public class App {
 		System.out.println("Hello Covid-19");
 		System.out.println("Hello Covid-19");
 		
-		Customer customer = new Customer ("Crystel", "Dwikat.M@ieee.org", 599149227,"123456*/-", "Nablus", "Jwwal Pay");
-		customer.showDetails();
+		Customer customer1 = new Customer ("Crystel", "Dwikat.M@ieee.org", 599149227,"123456*/-", "Nablus", "Jwwal Pay");
+		Customer customer2 = new Customer ("Imad", "Imad.S@ieee.org", 599149338,"789456*/-", "Tolkarm", "Cash");
+
+		customer1.showDetails();
+		customer2.showDetails();
 	}
 }
