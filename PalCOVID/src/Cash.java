2770b33c2ca81d758d69f8c2777400342a1139e7
Mohammad Dwikat <mohammed.dwikate1999@gmail.com>
2020-12-22 23:29:18 +0200

    add a new method for payment system

--- PalCOVID/src/Cash.java
+++ PalCOVID/src/Cash.java
@@ -8,4 +8,28 @@ public class Cash implements PaymentSystem  {
 		
 	}
 
+	@Override
+	public boolean checkValidity() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean checkBudget() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void makeDiscount() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void makeDeliveryFree() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
