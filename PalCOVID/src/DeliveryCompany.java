1fcca91a0419ba3143b3d5da82b8ea1bc2e79a49
Mohammad Dwikat <mohammed.dwikate1999@gmail.com>
2020-12-22 22:09:02 +0200

    apply singleton pattern on delivery company

--- PalCOVID/src/DeliveryCompany.java
+++ PalCOVID/src/DeliveryCompany.java
@@ -2,11 +2,24 @@
 public class DeliveryCompany {
 	private String name;
 	private int phoneNumber;
+	private volatile static DeliveryCompany uniqueInstance;
 	
-	public DeliveryCompany(String _1, int x) {
-		name = _1;
-		x = phoneNumber;
+	private DeliveryCompany(String _1, int x) {
+		this.name = _1;
+		this.phoneNumber = x ;
 	}
+	
+	public static DeliveryCompany getInstance(String name, int number) {
+		if(uniqueInstance == null) {
+			synchronized (DeliveryCompany.class){
+				if(uniqueInstance == null) {
+					uniqueInstance = new DeliveryCompany(name, number);
+				}
+			}
+		}
+		return uniqueInstance;
+	}
+	
 	public void setName(String n) {
 		name = n;
 	}
